42860bc34cbd98da79b1cfc4fac75f487b191042
pavidakshu <pavithra1665bpghss@gmail.com>
2026-07-08 22:07:16 +0530

    Delete SpringRestHandsonApplicationTests.java

--- SpringRestHandsonApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.cts.resthandson.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource("classpath:country-config.xml")
-public class XmlConfigImport {
-}
