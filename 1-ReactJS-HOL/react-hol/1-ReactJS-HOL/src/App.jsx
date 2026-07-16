990e23ccc369e4a4e186994937f07a29508bdbd6
pavidakshu <pavithra1665bpghss@gmail.com>
2026-07-16 09:09:58 +0530

    Add files via upload

---
+++ 1-ReactJS-HOL/react-hol/1-ReactJS-HOL/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '80px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Hello World!</h1>
+      <p>My first React application</p>
+    </div>
+  )
+}
+
+export default App
